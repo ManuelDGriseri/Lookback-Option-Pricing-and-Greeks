2957854bd25d9f9f6a05c649ed8abfa58ac514a3
Manuel Griseri <manuel.griseri@gmail.com>
2026-02-23 00:16:27 +0100

    Add files via upload

---
+++ DoxygenHTML/_option_8h.js
@@ -0,0 +1,5 @@
+var _option_8h =
+[
+    [ "opt::MCStats", "structopt_1_1_m_c_stats.html", "structopt_1_1_m_c_stats" ],
+    [ "opt::Option", "classopt_1_1_option.html", "classopt_1_1_option" ]
+];
---
+++ DoxygenHTML/classopt_1_1_aggregator.js
@@ -0,0 +1,4 @@
+var classopt_1_1_aggregator =
+[
+    [ "operator()", "classopt_1_1_aggregator.html#a190ce37fc68241c737903d640a0fc4dd", null ]
+];
